c72349f9e3aaa2b0708d5daefe271dd994d6bb33
Katie <cope_katie@yahoo.com>
2021-04-05 19:58:51 -0500

    part6 complete

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,13 @@ export class Satellite {
         this.operational = operational;
         this.launchDate = launchDate;
     }
-}
+
+    shouldShowWarning() {
+        if (this.type == "Space Debris") {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+};
